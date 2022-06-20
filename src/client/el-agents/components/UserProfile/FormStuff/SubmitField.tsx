aa872e754d8745c044aa445556dfea45f19c835c
andre-byrne <andre@edvisor.io>
2022-06-20 16:54:28 -0700

    Came back to this project after a while and it wouldn't start
     - had to fix some ts errors, change the api key, and rebuild

--- src/client/el-agents/components/UserProfile/FormStuff/SubmitField.tsx
+++ src/client/el-agents/components/UserProfile/FormStuff/SubmitField.tsx
@@ -2,6 +2,10 @@ import { Words } from '@atoms/components'
 import React, { HTMLProps, Ref } from 'react'
 import { Override, filterDOMProps, useForm } from 'uniforms'
 
+function isNil<T>(value: T | undefined): value is T {
+  return value !== undefined
+}
+
 export type SubmitFieldProps = Override<
   HTMLProps<HTMLInputElement>,
   { inputRef?: Ref<HTMLInputElement>; value?: string }
@@ -10,22 +14,29 @@ export type SubmitFieldProps = Override<
 export default function SubmitField({
   disabled,
   inputRef,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   readOnly,
   value,
   ...props
 }: SubmitFieldProps) {
-  const { error, state } = useForm()
+  const form = useForm()
+  const { state } = form
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const buttonProps: any = {
+    ...(!isNil(value) ? { value } : {}),
+    ...filterDOMProps(props),
+  }
 
   return (
     <div className='form-buttons-w text-right'>
       <button
+        { ...buttonProps }
         className='btn btn-primary'
-        disabled={disabled === undefined ? !!(error || state.disabled) : disabled}
-        readOnly={readOnly}
-        ref={inputRef}
-        type="submit"
-        {...(value ? { value } : {})}
-        {...filterDOMProps(props)}
+        disabled={disabled === undefined ? !!(!isNil(form.error) ? form.error : state.disabled) : disabled}
+        type='submit'
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-explicit-any
+        ref={inputRef as any}
       >
         <Words>agents:GLOBAL.SAVE</Words>
       </button>
